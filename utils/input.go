ae112c3b41d75e94aa58d6c5ae026557559ac19b
Foster Gorman <mh337.fg@gmail.com>
2024-11-30 10:28:24 -0500

    Finish input request get

--- utils/input.go
+++ utils/input.go
@@ -1,7 +1,26 @@
 package utils
 
-func GetDaysInput(day int) string {
-	in := "Test"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
-	return in
+const Year = 2024
+
+func GetDaysInput(day int) string {
+	sessionCookie := os.Getenv("SESSION_COOKIE")
+
+	url := fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", Year, day)
+
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+
+	req.Header.Add("Cookie", fmt.Sprintf("session=%s", sessionCookie))
+
+	res, _ := http.DefaultClient.Do(req)
+
+	resBody, _ := ioutil.ReadAll(res.Body)
+
+	return string(resBody)
 }
